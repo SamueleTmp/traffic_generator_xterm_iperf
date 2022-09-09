c226d436a2a4d5d823aadafca23c952acd78df2d
SamueleTmp <102318595+SamueleTmp@users.noreply.github.com>
2022-09-09 13:55:17 +0200

    Add files via upload

--- geant_topology.py
+++ geant_topology.py
@@ -74,6 +74,7 @@ def myNetwork():
     h21 = net.addHost('h21', cls=Host, ip='10.0.0.21', defaultRoute=None)
     h22 = net.addHost('h22', cls=Host, ip='10.0.0.22', defaultRoute=None)
     h23 = net.addHost('h23', cls=Host, ip='10.0.0.23', defaultRoute=None)
+    h24 = net.addHost('h24', cls=Host, ip='10.0.0.24', defaultRoute=None)
     
     
 
@@ -87,6 +88,7 @@ def myNetwork():
     net.addLink(s7, h7 )
     net.addLink(s8, h8 )
     net.addLink(s9, h9 )
+    net.addLink(s10, h24)
     net.addLink(s10, h10)
     net.addLink(s11, h11)
     net.addLink(s12, h12)
@@ -201,7 +203,7 @@ def myNetwork():
     info( '*** Post configure switches and hosts\n')
     dict1 ={}
     with open('./host_IP.json', 'w') as host_IP:
-     for i in range(23):
+     for i in range(24):
      	msg = 'h'+str(i+1)
      	host = net.get(msg)
      	ip = host.IP(None)
